e7dcc0d14f6c1b5025afaddaa2c8ae807b66f56a
sasithonfon <sasithonsaelao@gmail.com>
2021-10-21 22:32:41 +0700

    test naja

--- src/Fon.java
+++ src/Fon.java
@@ -1,6 +1,6 @@
 
 public class Fon {
 	public static void main(String[] args) throws Exception {
-        System.out.println("Hello, fon !");
+        System.out.println("Hello, fon asdfasdf!");
     }
 }
