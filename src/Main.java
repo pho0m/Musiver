736cd459a853a103162b215088ff070c57e4456c
pho0m <pho0m2000s@gmail.com>
2021-10-22 00:51:35 +0700

    refactor structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World !");
+        fonnajaaa();
+    }
+
+    public static void fonnajaaa() {
+        System.out.println("Hello, Pho0m !");
     }
 }
