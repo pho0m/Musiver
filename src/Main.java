c59aecf48460f96e001742352bfdc7b53b84fa0c
sasithonfon <sasithonsaelao@gmail.com>
2021-10-22 21:31:01 +0700

    Main in Project

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,41 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World !");
-        fonnajaaa();
-    }
+package application;
+import java.io.IOException;
 
-    public static void fonnajaaa() {
-        System.out.println("Hello, Pho0m !");
-    }
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+			@Override
+			public void handle(WindowEvent arg0) {
+				
+				Platform.exit();
+				System.exit(0);	
+			}		
+		});
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
 }
