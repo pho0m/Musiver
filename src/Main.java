ba8b424597ae38b1ab3007fb774dd75b75a4cfae
pho0m <pho0m2000s@gmail.com>
2021-10-29 21:19:51 +0700

    add example music

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
     stage.setScene(scene);
     stage.show();
     stage.setResizable(false);
+    stage.setTitle("Musiver TCT31");
   }
 
   public static void main(String[] args) {
